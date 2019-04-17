629a9c9beee20cbaabe49a3f633e1aec55a857f9
vansky <vansky@users.noreply.github.com>
2019-04-17 16:49:09 -0400

    marcc script

---
+++ pytorch/train_tf_template.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --partition=gpuk80
+#SBATCH --exclude=gpu019
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=6
+#SBATCH --job-name=TFXL
+#SBATCH --mail-type=end
+#SBATCH --mail-user=EMAIL
+#SBATCH --output=train_wt103.out
+#SBATCH --error=train_wt103.err
+#SBATCH --time=0-2:0:0
+
+module load python/3.6-anaconda
+source activate pytorch-1.0.0
+
+#EXCLUDE: git python
+module load cuda/8.0
+
+corpussize='200k';
+nhid=200;
+seed=$RANDOM;
+
+python train.py \
+        --work_dir WORKING_DIR \
+        --cuda \
+        --tgt_len 150 \
+        --mem_len 0 \
+        --eval_tgt_len 150 \
+        --batch_size 20 \
+	--trainfname wiki.train.tokens \
+	--validfname wiki.valid.tokens \
+	--testfname wiki.test.tokens \
+	--vocab_file grnn.vocab \
+        --gpu0_bsz 4 \
+	--attn_type 2 \
+	--seed ${seed} \
+        --data ../data/wikitext-103/ \
+        --dataset wt103 \
+        --n_layer 4 \
+        --d_model 400 \
+        --n_head 10 \
+        --d_head 41 \
+        --d_inner 2100 \
+        --dropout 0.2 \
+        --dropatt 0.0 \
+        --optim adam \
+        --lr 0.00025 \
+        --warmup_step 0 \
+        --max_step 200000 \
+        --multi_gpu
+
